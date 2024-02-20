bbba01196964d041986ead66679dfe96584ab855
Patrick McElhaney <pmcelhaney@gmail.com>
2024-02-20 16:42:23 -0500

    increase test coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,10 @@ export default {
 
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 85,
+      functions: 94,
+      lines: 94,
+      statements: 94,
     },
   },
 
